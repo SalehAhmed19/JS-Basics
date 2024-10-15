daee12e1244ec871d38cdf635e446c5ce016ca78
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2024-10-16 03:48:46 +0600

    JS basic operation 1

---
+++ operation.js
@@ -0,0 +1,9 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+console.log(eggPrice + onionPrice); // addition operation
+console.log(onionPrice - eggPrice); // subtraction operation
+console.log(onionPrice * eggPrice); // multiplication operation
+console.log(onionPrice / eggPrice); // division operation
+console.log(10 / 3); // division operation
+console.log(10 % 3); // modulus operation
