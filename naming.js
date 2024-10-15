702dc4bc2d6d26cfd82b44265458b11f83557b21
Saleh Ahmed Mahin <saleh.ahmed.mahin@gmail.com>
2024-10-16 03:30:33 +0600

    JS variable naming style

---
+++ naming.js
@@ -0,0 +1,23 @@
+// 1. No keyword in variable name
+var false = 45;
+var var = 50;
+
+// 2. No space or gap in variable name
+var is happy = false;
+
+// 3. No quote
+var "address" = "Registry Office RD, Sanyal Para, Sherpur, Bogura";
+
+// 4. Can not start with a number. But number other than the first letter is a allowed
+var 1money = 45;
+var money1 = 45;
+
+// Name is case sensitive
+var address = "Registry Office RD, Sanyal Para, Sherpur, Bogura";
+var Address = "Registry Office RD, Sanyal Para, Sherpur, Bogura";
+
+// 6. How to write a long variable name
+var my current home address = "Registry Office RD, Sanyal Para, Sherpur, Bogura";
+var my_current_home_address = "Registry Office RD, Sanyal Para, Sherpur, Bogura"; // Snake case
+var myCurrentHomeAddress = "Registry Office RD, Sanyal Para, Sherpur, Bogura"; // Camel case
+var MyCurrentHomeAddress = "Registry Office RD, Sanyal Para, Sherpur, Bogura"; // Pascal case
